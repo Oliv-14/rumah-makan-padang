f8b2896b41725fff2f077f2b1d7c8432dcaebd68
Oliv-14 <sulistianaolivia@gmail.com>
2025-11-10 10:06:39 +0700

    Ganti ke HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 // 1. Impor BrowserRouter untuk mengaktifkan routing
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
-import App from './App.jsx';
+import App from "./App.jsx";
 
 // 2. Impor CSS Bootstrap agar tersedia di seluruh aplikasi
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // Hapus CSS default Vite jika ada, atau buat file baru jika perlu styling global
-// import './index.css'; 
+// import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* 3. Bungkus komponen <App /> dengan <BrowserRouter> */}
+    {/* 3. Bungkus komponen <App /> dengan <HashRouter> */}
     {/*    Ini adalah langkah krusial yang membuat semua komponen routing berfungsi. */}
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
